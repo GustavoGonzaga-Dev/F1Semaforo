2c20c4c3e536e8b7ac0009908b6a6102ad551931
dev <dev@dev-RV411-RV511-E3511-S3511-RV711-E3411>
2020-09-23 22:51:32 -0300

    anotações

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,7 +1,14 @@
 package view;
 
+import java.util.Random;
+
 public class Principal {
 	public static void main(String[] args) {
+		Random random = new Random();
+		//O circuito durara 3 voltas
+		//7 times, cada time tem 2 carros (14 carros no total). 
+		//dos 7 times, só 5 irão participar do circuito. "criar um random pra decidir quais times participarão"
+		//Só 5 carros podem entrar na pista simultaneamente, sendo um carro de cada time.
 		
 	}
 
